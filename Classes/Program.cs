2a7d8812472310114071036750076c0589b3decb
gorkemakpur <gorkemakpur@gmail.com>
2023-10-24 23:12:04 +0300

    Class workouts

---
+++ Classes/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager();
+            ProductManager productManager = new ProductManager();
+            productManager.Update();
+            customerManager.Add();
+
+            Customer customer = new Customer();
+            customer.FirstName = "Test";
+            customer.LastName = "Test-2";
+            customer.City = "İstanbul";
+            customer.ID = 1;
+
+            Customer customer1 = new Customer
+            {
+                FirstName = "2-Test",
+                LastName = "2-Test2",
+                City = "Ankara",
+                ID = 2
+            };
+
+            Console.WriteLine(customer.FirstName);
+
+            Console.ReadLine();
+        }
+    }
+
+
+}
